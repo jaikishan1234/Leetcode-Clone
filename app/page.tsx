7bbcf603bfec741b2d09e616f9b381456d580407
Jai <jaikishannayak@gmail.com>
2026-05-29 12:56:31 +0530

    Authentication Using Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Click
-      </Button>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <UserButton />
     </div>
   );
 }
